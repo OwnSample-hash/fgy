1c3907bfc9ec4bc4ddd74f42d11e6c5381c759e5
default <ownsample@tutanota.com>
2025-09-28 20:45:18 +0000

    Logging middleware

---
+++ backend/include/middlewares/logging.hpp
@@ -0,0 +1,32 @@
+#pragma once
+#include <chrono>
+#include <format>
+#include <iostream>
+#include <middleware.hpp>
+
+using namespace std::literals;
+
+class LoggingMiddleware : public IMiddleware {
+  std::ostream &out = std::cout;
+  std::string path = "*";
+  std::string UserAgent = "";
+
+public:
+  LoggingMiddleware() = default;
+  LoggingMiddleware(std::ostream &out) : out(out) {}
+  ~LoggingMiddleware() override = default;
+  MiddlewareResult before(req &request, const endpoint_type &) override {
+    UserAgent = request[http::field::user_agent];
+    return CONTINUE;
+  }
+  MiddlewareResult after(res &response, const endpoint_type &rep) override {
+    auto now = std::chrono::system_clock::now();
+    out << rep.address().to_string() << " - ["
+        << std::format("{:%d}/{:%b}/{:%Y}:{:%H}:{:%M}:{:%S0} {:%z}", now, now,
+                       now, now, now, now, now)
+        << "] \"" << response.result_int() << "\"" << response.payload_size()
+        << " - \"" << UserAgent << "\"\n";
+    return CONTINUE;
+  }
+  MiddlewareFire when() override { return BOTH; }
+};
