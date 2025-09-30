a268b1fcea7a98507c9ed4acbaeaf02356f629ec
default <ownsample@tutanota.com>
2025-09-30 20:07:02 +0000

    fix: const-ify the `req` and `res` parameters

--- backend/include/middleware.hpp
+++ backend/include/middleware.hpp
@@ -7,11 +7,13 @@ enum MiddlewareFire { BEFORE = 1, AFTER, BOTH = BEFORE | AFTER };
 class IMiddleware {
 public:
   virtual ~IMiddleware() = default;
-  virtual MiddlewareResult before(req &, const endpoint_type &) {
+  virtual MiddlewareResult before(const req &, const endpoint_type &) {
     return CONTINUE;
   }
-  virtual MiddlewareResult after(res &, const endpoint_type &) {
+  virtual MiddlewareResult after(const res &, const endpoint_type &) {
     return CONTINUE;
   }
   virtual MiddlewareFire when() = 0;
+#warning                                                                       \
+    "Add and use a path property to limit middleware application to specific routes"
 };
