040825f1c040029ac1f94f309f71510d9cf97512
OwnSample <ownsample@tutanota.com>
2025-10-24 10:32:39 +0200

    feat: action jobs dockerifyd maine

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 import os
+from dotenv import load_dotenv
+
+
+if os.getenv("ENV") == "PROD":
+    load_dotenv("data/.env")
+else:
+    load_dotenv()
+
+
 from fastapi import FastAPI
 from db import db
-from dotenv import load_dotenv
 from auth import router as auth_router
 from sys import argv
 import uvicorn
 
-load_dotenv()
 
 app = FastAPI()
 app.include_router(auth_router, tags=["auth"], prefix="/api")
@@ -20,11 +27,15 @@ async def read_root():
 
 if __name__ == "__main__":
     if "--do-db" in argv:
-        db.drop_tables()
-        db.create_tables()
+        try:
+            db.drop_tables()
+            db.create_tables()
+            print("Database setup completed.")
+        except Exception as e:
+            print(f"Error setting up database: {e}")
 
 
     if "--reload" in argv:
         uvicorn.run("main:app", host="127.0.0.1", port=int(os.getenv("PORT", 8000)), reload=True)
     else:
-        uvicorn.run(app, host="127.0.0.1", port=int(os.getenv("PORT", 8000)))
+        uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
